9211bdfaa364f8a13733dbbf7948a476dd7659ca
hugo.varloud <hugo.varloud@epitech.eu>
2017-06-30 20:47:54 +0200

    sortby

--- src/sortbye.c
+++ src/sortbye.c
@@ -5,11 +5,31 @@
 ** Login   <romain.pillot@epitech.net>
 ** 
 ** Started on  Fri Jun 30 18:25:09 2017 romain pillot
-** Last update Fri Jun 30 18:25:30 2017 romain pillot
+** Last update Fri Jun 30 20:41:40 2017 hugo varloud
 */
 
 int	sort_by_name(const void *a, const void *b)
 {
 
+  return (0);
+}
+
+int	sort_by_forename(const void *a, const void *b)
+{
+  return (0);
+}
+
+int	sort_by_job(const void *a, const void *b)
+{
+  return (0);
+}
+
+int	sort_by_zip(const void *a, const void *b)
+{
+  return (0);
+}
+
+int	sort_by_id(const void *a, const void *b)
+{
   return (0);
 }
--- src/sortbym.c
+++ src/sortbym.c
@@ -5,6 +5,22 @@
 ** Login   <romain.pillot@epitech.net>
 ** 
 ** Started on  Fri Jun 30 18:25:43 2017 romain pillot
-** Last update Fri Jun 30 18:25:47 2017 romain pillot
+** Last update Fri Jun 30 20:42:28 2017 hugo varloud
 */
 
+int	sort_by_date(int id)
+{
+  
+  return (0);
+}
+
+int	sort_by_zip(int id)
+{
+  
+  return (0);
+}
+
+int	sort_by_id(int id)
+{
+  return (0);
+}
